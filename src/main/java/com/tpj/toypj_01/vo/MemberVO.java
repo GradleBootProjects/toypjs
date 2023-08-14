f8a57ce50766154c1c418bf7159791f21250629d
ben <loveeld132@naver.com>
2023-08-14 17:34:50 +0900

    * Mybatis Test
    - Please Edit Controller & FrontEnd

--- src/main/java/com/tpj/toypj_01/vo/MemberVO.java
+++ src/main/java/com/tpj/toypj_01/vo/MemberVO.java
@@ -1,5 +1,11 @@
 package com.tpj.toypj_01.vo;
 
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Getter
+@Setter
 public class MemberVO {
     public String name;
     public int win;
@@ -13,4 +19,24 @@ public class MemberVO {
         this.lose = lose;
     }
 
+    public String getName() {
+        return name;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public int getWin() {
+        return win;
+    }
+    public void setWin(int win){
+        this.win = win;
+    }
+
+    public int getLose() {
+        return lose;
+    }
+    public void setLose(int lose){
+        this.lose = lose;
+    }
 }
